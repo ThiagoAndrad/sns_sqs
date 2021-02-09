739287c6fda9a4f527d9df383d454e1bddf2190f
thpandrade <thiago.andrade@loft.com.br>
2021-02-08 21:35:42 -0300

    initial commit

---
+++ src/main/kotlin/com/thpandrade/sns_sqs/SnsSqsApplication.kt
@@ -0,0 +1,11 @@
+package com.thpandrade.sns_sqs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnsSqsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnsSqsApplication>(*args)
+}
